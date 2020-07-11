e5106c844e19f6e7ed1dcc926185d30912097b9d
Paul Miller <miller_p@hotmail.fr>
2020-07-11 12:09:17 +0200

    Implemented google trends use and non-working code for differenced input

--- README.md
+++ README.md
@@ -1,33 +1,43 @@
 # Stock prediction
 
 ## Description
-The goal of this project is to predict the value of a stock for the next day and use the data to simulate a trading algorithm. I chose to use the average of the High and Low values for the stock to train a LSTM model.
+The goal of this project is to predict the value of a stock for the next day in hopes of maybe simulating a trading algorithm. I chose to use the average of the High and Low values for the stock to train a LSTM model, working only on 3 stocks for now and to analyze the problems which arise.
 
 ## Results
 Here we can see an example of the next day predictions for 3 stocks.
-Parameters : using the last 20 days for the predictions, 25 epochs of training, 200 hidden neurons for the LSTM
+Parameters : using the last 20 days as input sequence for the predictions, 25 epochs of training, 200 hidden neurons for the LSTM
 
 ![Microsoft](microsoft_20in_25ep_200h.png)
 ![Apple](apple_20in_25ep_200h.png)
 ![Amazon](amazon_20in_25ep_200h.png)
 
-Microsoft RMSE : 5.138481244485672
-Apple RMSE : 10.82418362089971
-Amazon RMSE : 82.2145559000393
+- Amazon RMSE : 61.98837186223845
+- Apple RMSE : 8.299626607550193
+- Microsoft RMSE : 6.541609455255556
 
 We see that the model can predict the next day's mean stock value pretty well for some stocks (i.e. low error), but what is more important to us is that we don't predict it to go up when it goes down.
 
 If we only take the binary result of up or down from this model, we get these results :
-Amazon : 9 wrong predictions out of 20
-Apple : 11 wrong predictions out of 20
-Microsoft : 15 wrong predictions out of 20
+Amazon : 6 wrong predictions out of 20
+Apple : 9 wrong predictions out of 20
+Microsoft : 9 wrong predictions out of 20
+
+Even though the results seem okay when seen like this, these results don't have much value. For example, we can see that we always predict an increase for the Amazon stock.
+
+
+## How I am trying to improve the model
+I am trying to see if I can find a correlation between google searches for specific keywords and the value of a stock. For example, if the days after searches for the name of the company and the word "scandal" increase, the stock might go down since people might tend to sell.
+
+I am also trying to use the differenced dataframe to train the network, which may lead to a better result in the binary task of predicting "up" or "down" for the next day and would eliminate the need for smoothing windows
+
 
 ## Further work
-- Use the differenced dataframe to train the network, which may lead to a better result in the binary task of predicting "up" or "down" for the next day and would eliminate the need for smoothing windows
 - Try building a binary classification model (i.e. only predicting "up" or "down")
 - Try using different stocks
 - Tune hyperparameters (hidden layer size, input sequence length, training epochs, learning rate, etc.)
 - Try a different LSTM architecture (for example 2 hidden layers)
 - Clean code
 - Find a way to figure out how sure the model is of its prediction
-- Integrate other data to train the model (for example a Google Trend search for the company using pytrends)
+
+
+
--- forecast.py
+++ forecast.py
@@ -8,6 +8,40 @@ import torch
 import torch.nn as nn
 from statsmodels.tools.eval_measures import rmse
 import datetime
+from pytrends.request import TrendReq
+from pytrends import dailydata
+
+
+def over_time(kw_list, draw=True, **kwargs):
+    """
+    Fetches the DataFrame of the Google Trend for a list of keywords. By
+    default, it will draw the plot.
+
+    Parameters
+    ----------
+    kw_list : list
+        List of keywords to search.
+    draw : bool, optional
+        Draw the bar plot for the keywords or not. The default is True.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing the keywords as columns and the country as index.
+
+    """
+    pytrend = TrendReq()
+    
+    pytrend.build_payload(kw_list=kw_list, **kwargs)
+    
+    df = pytrend.interest_over_time()
+    
+    df = df.drop('isPartial', axis=1)
+    
+    if draw:
+        df.plot()
+    
+    return df
 
 
 def preprocess(history_filename, cols_w_dollarsign, cols2drop=[],
@@ -35,15 +69,17 @@ def remove_dollarsign(df, col_name):
 
 class LSTM(nn.Module):
     """Class for the LSTM model"""
-    def __init__(self, input_size=2, hidden_layer=200, n_input=20):
+    def __init__(self, input_size=3, hidden_layer=200, output_size=3,
+                 n_input=20):
         super().__init__()
         self.input_size = input_size
         self.hidden_layer = hidden_layer
+        self.output_size = output_size
         self.n_input = n_input
         
         self.lstm = nn.LSTM(input_size, hidden_layer)
         
-        self.linear = nn.Linear(hidden_layer * self.n_input, input_size)
+        self.linear = nn.Linear(hidden_layer * self.n_input, output_size)
         
         self.hidden_cell = (torch.zeros(1, 1, self.hidden_layer),
                             torch.zeros(1, 1, self.hidden_layer))
@@ -54,81 +90,12 @@ class LSTM(nn.Module):
         return predictions
 
 
-if __name__ == '__main__':
-    cols2drop = ['Open', 'Close/Last', 'Volume', 'High', 'Low']
-    amazon = preprocess('AmazonHistory.csv', ['High', 'Low'], cols2drop)
-    apple = preprocess('AppleHistory.csv', ['High', 'Low'], cols2drop)
-    microsoft = preprocess('MicrosoftHistory.csv', ['High', 'Low'], cols2drop)
-    amazon.columns = ['Amazon']
-    df = amazon
-    df['Apple'] = apple
-    df['Microsoft'] = microsoft
-    
-    diffed = False
-    n_input = 20
-    epochs = 25
-    input_size = 3
-    n_preds = 1
-    test_size = 20
-    hidden_layer = 200
-    
-    model = LSTM(n_input=n_input, input_size=input_size,
-                 hidden_layer=hidden_layer)
-    loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-
-    train = df.iloc[:-test_size].copy()
-    test = df.iloc[-test_size:].copy()
-    
-    # Data differencing
-    if diffed:
-        train = train.diff()[1:]
-        last_val = df.iloc[len(train)-1]
-    
-    scaler = MinMaxScaler()
-    
-    # Here we choose a smoothing window so that values are scaled within their
-    # own time period to avoid that early low stock data has little impact
-    smoothing_window = 500
-    for i in range(0, 2000, smoothing_window):
-        scaler.fit(train.iloc[i:i+smoothing_window,:])
-        train.iloc[i:i+smoothing_window,:] = scaler.transform(
-            train.iloc[i:i+smoothing_window,:])
-
-    scaler.fit(train.iloc[i+smoothing_window:,:])
-    train.iloc[i+smoothing_window:,:] = scaler.transform(
-        train.iloc[i+smoothing_window:,:])
-    
-    test_sc = scaler.transform(test)
-    
-    inputs = []
-
-    for i in range(n_input, len(train)):
-        inputs.append((train.iloc[i-n_input:i].values, train.iloc[i].values))
-    
-    model.train()
-    losses = []
-
-    for i in range(epochs):
-        for seq, label in inputs:
-            optimizer.zero_grad()
-            model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer),
-                                 torch.zeros(1, 1, model.hidden_layer))
-            seq = torch.FloatTensor(seq)
-            label = torch.FloatTensor(label).reshape(-1, input_size)
-            seq = seq.reshape(-1, 1, input_size)
-            y_pred = model(seq)
-            loss = loss_fn(y_pred, label)
-            loss.backward()
-            optimizer.step()
-    
-        L = loss.item()
-        losses.append(L)
-        print('epoch: {} loss: {:.7f}'.format(i, L))
-    
-    current_batch = train.iloc[-test_size:].values
-    
+def evaluate(model, train_w_trends, test_w_trends, test_size, scaler,
+             diffed=False, df=None):
     model.eval()
+            
+    current_batch = train_w_trends.iloc[-test_size:].values
+
     preds = None
     
     for i in range(test_size-n_preds+1):
@@ -145,19 +112,213 @@ if __name__ == '__main__':
             else:
                 preds = np.append(preds, pred.detach().numpy(), axis=0)
         
-            current_batch = torch.cat((current_batch[1:],
-                                       pred.detach().reshape(-1, 1, input_size)))
+            if n_preds > 1:
+                current_batch = torch.cat((current_batch[1:],
+                                           pred.detach().reshape(
+                                               -1, 1, input_size)))
     
-        if i < test_size-n_preds-1:
-            current_batch = np.append(train.iloc[-test_size+i+1:].values,
-                                      test_sc[:i+1], axis=0)
+        if i < test_size-n_preds:
+            """
+            print('{}-th test batch will use dates from {} to {}'.format(
+                i + 1, train.index[-test_size+i+1].strftime('%Y-%m-%d'),
+                test.index[i].strftime('%Y-%m-%d')))
+            """
+            current_batch = np.append(
+                train_w_trends.iloc[-test_size+i+1:].values,
+                test_w_trends[:i+1], axis=0)
     
-    preds_unsc = scaler.inverse_transform(preds.reshape(-1, input_size))
+    stock_values = preds[:, :len(stocks)]
+    preds_unsc = scaler.inverse_transform(stock_values)
     
+    if diffed:
+        preds_undiff = np.append(
+            df.iloc[-test_size - 1].values.reshape(-1, 3), preds_unsc, axis=0)
+        preds_undiff = preds_undiff.cumsum(axis=0)[1:]
+        
+        return preds_undiff
+    
+    return preds_unsc
+
+
+def get_binary_errors(preds_unsc, test, stocks, dfdiff, diffed):
+    preds = pd.DataFrame(preds_unsc, index=test.index, columns=stocks)
+    if not diffed:
+        preds_diff = ((preds - df.iloc[-len(test)-1:-1].values)>0).astype(int)
+    preds_diff = preds > 0
+    
+    dfdiff = dfdiff - preds_diff
+    for stock in stocks:
+        print('{} : {} wrong predictions out of {}'.format(
+            stock, int(dfdiff[stock].abs().sum()), len(test)))
+
+
+if __name__ == '__main__':
+    cols2drop = ['Open', 'Close/Last', 'Volume', 'High', 'Low']
+    dollarsign_cols = ['High', 'Low']
     stocks = ['Amazon', 'Apple', 'Microsoft']
-    colors = ['blue', 'orange', 'green']
+    for i in range(len(stocks)):
+        if i == 0:
+            df = preprocess(stocks[i] + 'History.csv',
+                            dollarsign_cols,
+                            cols2drop)
+        else:
+            df = pd.concat([df, preprocess(stocks[i] + 'History.csv',
+                                           dollarsign_cols,
+                                           cols2drop)], axis=1)
     
-    for i in range(input_size):
+    df.columns=stocks
+    
+    kw_search = []
+    
+    """
+    To create the Google Trend CSV files for each stock
+    for i in range(len(stocks)):
+        kw = stocks[i] + ' stock'
+        daily = dailydata.get_daily_data(kw, 2010, 7, 2020, 7).iloc[5:-5]
+        daily[[kw + '_unscaled']].to_csv(stocks[i] + 'Trend.csv')
+    """
+    
+    """
+    Notes: this is in testing, multiple combinations won't work. For example,
+    the diffed attribute doesn't work properly
+    """
+    
+    use_trends = False
+    diffed = False
+    n_input = 20
+    epochs = 25
+    eval_every = 5
+    input_size = len(stocks)
+    if use_trends:
+        input_size += len(stocks)
+    output_size = input_size
+    n_preds = 1
+    test_size = 20
+    hidden_layer = 200
+    lr = 1e-3
+    kw = 'scandal'
+    
+    if use_trends:
+        for i in range(len(stocks)):
+            if i == 0:
+                trends = pd.read_csv(stocks[i] + kw.capitalize() + '.csv',
+                                     index_col='date', parse_dates=True)
+            else:
+                trends = pd.concat([trends, pd.read_csv(
+                    stocks[i] + kw.capitalize() +'.csv', index_col='date',
+                    parse_dates=True)], axis=1)
+    
+    # Data differencing
+    if diffed:
+        train = df.iloc[:-test_size].diff()[1:]
+    else:
+        train = df.iloc[:-test_size].copy()
+        
+    test = df.iloc[-test_size:].copy()
+        
+    if use_trends:
+        trends_train = trends.loc[:test.index.min()].copy()
+        trends_test = trends.loc[train.index.max():].copy()
+        
+        scaler_trends = MinMaxScaler()
+        trends_train.iloc[:, :] = scaler_trends.fit_transform(
+            trends_train).round(4)
+        trends_test.iloc[:, :] = scaler_trends.transform(trends_test).round(4)
+    
+    model = LSTM(n_input=n_input, input_size=input_size,
+                 hidden_layer=hidden_layer, output_size=output_size)
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    
+    scaler = MinMaxScaler()
+    
+    # Here we choose a smoothing window so that values are scaled within their
+    # own time period to avoid that early low stock data has little impact
+    if not diffed:
+        smoothing_window = 500
+        for i in range(0, 2000, smoothing_window):
+            scaler.fit(train.iloc[i:i+smoothing_window,:])
+            train.iloc[i:i+smoothing_window,:] = scaler.transform(
+                train.iloc[i:i+smoothing_window,:]).round(4)
+    
+        scaler.fit(train.iloc[i+smoothing_window:,:])
+        train.iloc[i+smoothing_window:,:] = scaler.transform(
+            train.iloc[i+smoothing_window:,:]).round(4)
+        
+        if use_trends:
+            train_w_trends = pd.concat([train, trends_train], axis=1, join='inner')
+        else:
+            train_w_trends = train
+        
+        scaled_values = scaler.transform(test)
+        test_sc = test.copy()
+        for i in range(len(stocks)):
+            test_sc.iloc[:, i] = scaled_values[:, 0]
+    
+    else:
+        train.iloc[:, :] = scaler.fit_transform(train)
+        test_sc = test.copy()
+        test_sc.iloc[:, :] = scaler.transform(test)
+    
+    if use_trends:
+        test_w_trends = pd.concat([test_sc, trends_test], axis=1, join='inner')
+    else:
+        train_w_trends = train
+        test_w_trends = test_sc
+    
+    df4diff = df.iloc[len(train)-1:len(train)].copy()
+    dfdiff = df4diff.append(test)
+    dfdiff = (dfdiff - dfdiff.shift(1)).dropna()
+    dfdiff = (dfdiff>0).astype(int)
+    
+    inputs = []
+
+    for i in range(n_input, len(train)):
+        inputs.append((train_w_trends.iloc[i-n_input:i].values,
+                       train_w_trends.iloc[i].values))
+    
+    losses = []
+
+    for i in range(epochs):
+        if i % eval_every == 0:
+            model.train()
+        for seq, label in inputs:
+            optimizer.zero_grad()
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer),
+                                 torch.zeros(1, 1, model.hidden_layer))
+            seq = torch.FloatTensor(seq)
+            label = torch.FloatTensor(label).reshape(-1, input_size)
+            seq = seq.reshape(-1, 1, input_size)
+            y_pred = model(seq)
+            loss = loss_fn(y_pred, label)
+            loss.backward()
+            optimizer.step()
+    
+        L = loss.item()
+        losses.append(L)
+        print('epoch: {}/{} loss: {:.7f}'.format(i+1, epochs, L))
+        
+        if (i + 1) % eval_every == 0:
+            preds_unsc = evaluate(model, train_w_trends, test_w_trends,
+                                  test_size, scaler)
+            get_binary_errors(preds_unsc, test, stocks, dfdiff, diffed)
+    
+    if (i + 1) % eval_every != 0:
+        preds_unsc = evaluate(model, train_w_trends, test_w_trends, test_size,
+                              scaler)
+        get_binary_errors(preds_unsc, test, stocks, dfdiff, diffed)
+    
+    colors = ['blue', 'orange', 'green']
+
+    preds = pd.DataFrame(preds_unsc, index=test.index, columns=stocks)
+    
+    if diffed:
+        for stock in stocks:
+            preds = pd.concat([df.iloc[len(train):len(train)+1], preds], axis=0)
+            preds[stock] = preds[stock].cumsum()
+            preds = preds.iloc[1:]
+        
+    for i in range(len(stocks)):
         plt.figure('{}_{}in_{}ep_{}h'.format(stocks[i].lower(),
                                              n_input,
                                              epochs,
@@ -174,23 +335,15 @@ if __name__ == '__main__':
             x = test.index[j:j+n_preds].insert(0, day_before)
 
             y = np.append(df.loc[day_before].values[i],
-                          preds_unsc[n_preds*j:n_preds*j+n_preds, i])
+                          preds.iloc[n_preds*j:n_preds*j+n_preds, i])
             
             plt.title(title)
             plt.plot(x, y, color=colors[i], linestyle='--', label=label)
         
         plt.show()
-
-    df4diff = df.iloc[len(train)-1:len(train)].copy()
-    dfdiff = df4diff.append(test)
-    dfdiff = (dfdiff - dfdiff.shift(1)).dropna()
-    dfdiff = (dfdiff>0).astype(int)
-
-    preds = pd.DataFrame(preds_unsc, index=test.index, columns=stocks)
-    preds_diff = ((preds - df.iloc[len(train)-1:-1].values)>0).astype(int)
     
-    dfdiff = dfdiff - preds_diff
+    preds_diff = (preds_unsc > 0).astype(int)
+    
     for stock in stocks:
-        print('{} : {} wrong predictions out of {}'.format(
-            stock, int(dfdiff[stock].abs().sum()), test_size))
+        print('{} RMSE : {}'.format(stock, rmse(test[stock], preds[stock])))
 
